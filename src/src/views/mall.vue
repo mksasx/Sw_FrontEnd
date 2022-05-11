970905be715aa7fe55bc8ae67fe82714ecdd89fd
Ma <1263122871@qq.com>
2022-05-11 12:55:40 +0800

    try

---
+++ src/src/views/mall.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>房源管理</div>
+</template>
+
+<script>
+export default {
+  name: "Mall",
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/src/views/self.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="a">
+      <p>个人中心</p>
+      <p>{{ msg }}</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    name:"Self",
+    data () {
+        return {
+             msg:'ma'
+        }
+    },
+}
+</script>
+
+<style scoped>
+ .a {
+     font-weight: bold;
+     text-align: center;
+ }
+
+</style>
